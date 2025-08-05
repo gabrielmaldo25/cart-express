4d0a8af22ddca2d2e447bb1e7f946a0f84c5cd95
gabrielmaldo25 <maldoangel@hotmail.com>
2025-08-04 21:58:58 -0300

    add errorHandler middleware

--- src/app.js
+++ src/app.js
@@ -9,19 +9,21 @@ const authRoutes = require("./routes/authRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const productRoutes = require("./routes/productRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 app.use(cors());
+app.use(errorHandler)
 
 const PORT = 3000;
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
-/* app.use("/api/cart", cartRoutes);
-app.use("/api/payment", paymentRoutes);
-app.use("/api/products", productRoutes); */
+//app.use("/api/cart", cartRoutes);
+//app.use("/api/payment", paymentRoutes);
+app.use("/api/products", productRoutes);
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto: ${PORT}`);
 });
